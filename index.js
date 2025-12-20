e22f81efe4eb8d9f6fcd404b3a728d918d448479
Rimonkumar <22-46957-1@student.aiub.edu>
2025-12-20 10:02:06 +0600

    create a basic server setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Coffee server id geating hotter')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
